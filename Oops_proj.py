e66b71f8809fd42d84b4bf4af57d4d77b2992d1b
Gaurav Pathak <pathakgauarv21june@gmail.com>
2025-06-01 16:10:38 +0530

    signup and signin feature added

--- Oops_proj.py
+++ Oops_proj.py
@@ -4,6 +4,7 @@ class Chatboat:
         self.username=""
         self.passwrd=""
         self.loggedin=False
+        self.credentials_history={}
         self.menu()
 
     def menu(self):
@@ -14,15 +15,39 @@ class Chatboat:
                             4.Press 4 to message a friend
                             5.Press any other key to exit\n""")
         if user_input=="1":
-            pass
+            self.signup()
         elif user_input=="2":
-            pass
+            self.signin()
         elif user_input=="3":
             pass
         elif user_input=="4":
             pass
         else:
             exit()
+    
+
+    def signup(self):
+        self.username=input("Enter your email :-->  ")
+        self.passwrd=input("Enter your password :-->  ")
+        self.credentials_history[self.username]=self.passwrd
+        print("You have successfully signed up!")
+        print("\n")
+        self.menu()
+    
+    def signin(self):
+        if self.username=="" or self.passwrd=="":
+            print("Please signup first!")
+            self.menu()
+        else:
+            user_input=input("Enter your email :-->  ")
+            pass_input=input("Enter your password :-->  ")
+            if self.credentials_history.get(user_input) == pass_input:
+                print("You have successfully signed in!")
+                self.loggedin=True
+            else:
+                print("Invalid credentials, please try again.")
+        print("\n")
+        self.menu()
 
 
 
